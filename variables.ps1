f91945bdc544462e7542a82e7e163da6091dc56a
Stone <info@simracingjustfair.org>
2024-07-24 21:48:15 +0200

    removed not needed variables

--- variables.ps1
+++ variables.ps1
@@ -9,18 +9,3 @@ $RF2ROOT="$HOME\rf2ds"
 
 # where do we find the downloaded workshop packages
 $RF2WORKSHOPPKGS="$RF2ROOT\steamapps\workshop\content\365960"
-
-# as SteamCMD needs to be installed, where to find it
-$STEAMINSTALLDIR="$RF2ROOT\steamcmd"
-
-# we need an Steam API key for the DLC installer
-$STEAMAPIKEY=""
-
-# DLC installer uses CSV files ...
-$CSVCARFILE="$RF2ROOT\dlccars.csv"
-$CSVTRACKFILE="$RF2ROOT\dlctracks.csv"
-$CSVCONTENTFILE="$RF2ROOT\dlccontent.csv"
-
-# name of the profile to use (refer to $RF2ROOT\Userdata\<profile>)
-# note: can be given as argument on CLI where used in scripts
-$PROFILE="player"
